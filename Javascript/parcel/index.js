dce842898fa2b2e387e20a99a09ebe83681bd15c
Domenico Maisano <dev.dmaisano@gmail.com>
2018-06-15 07:32:03 -0400

    new folder structure

---
+++ Javascript/parcel/index.js
@@ -0,0 +1,13 @@
+import { jokes } from './jokes';
+import $ from 'jquery';
+import fs from 'fs';
+
+jokes.getJoke()
+  .then(joke => {
+    // document.getElementById('joke').innerHTML = joke;
+    $('#joke').text(joke);
+  });
+
+const copy = fs.readFileSync(__dirname + '/copyright.txt', 'utf8');
+
+$('#copy').text(copy);

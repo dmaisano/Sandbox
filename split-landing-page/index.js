a36d40d757c5e5ae000b5765907fb750206aedd7
Domenico Maisano <dev.dmaisano@gmail.com>
2018-08-28 21:11:58 -0400

    revised files

--- split-landing-page/index.js
+++ split-landing-page/index.js
@@ -3,18 +3,17 @@ const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 
 left.addEventListener('mouseenter', () => {
-  container.classList.add('hover-left');
+	container.classList.add('hover-left');
 });
 
 left.addEventListener('mouseleave', () => {
-  container.classList.remove('hover-left');
+	container.classList.remove('hover-left');
 });
 
-
 right.addEventListener('mouseenter', () => {
-  container.classList.add('hover-right');
+	container.classList.add('hover-right');
 });
 
 right.addEventListener('mouseleave', () => {
-  container.classList.remove('hover-right');
+	container.classList.remove('hover-right');
 });
